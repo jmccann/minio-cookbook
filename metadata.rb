cec6f0ea545530d31c07a3a867587b31a4cfcf88
Patrick Schaumburg <pschaumburg@tecracer.de>
2019-10-11 09:19:19 +0200

    update all necessary files to align with current state

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,15 @@
 name             'minio'
-maintainer       'Jacob McCann'
-maintainer_email 'jacob.mccann2@target.com'
-license          'all_rights'
-description      'Installs/Configures minio'
+maintainer       'Patrick Schaumburg'
+maintainer_email 'info@p-schaumburg.de'
+license          'Apache-2.0'
+description      'Installs/Configures minio server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url       'https://github.com/jmccann/minio-cookbook'
-issues_url       'https://github.com/jmccann/minio-cookbook/issues'
-version          '0.1.0'
+source_url       'https://github.com/tecracer/minio-cookbook'
+issues_url       'https://github.com/tecracer/minio-cookbook/issues'
+version          '0.2.0'
+
+chef_version '>= 14.0'
 
 depends 'jmccann-docker-host', '~> 0.3'
+
+supports 'ubuntu', '= 16.04'
